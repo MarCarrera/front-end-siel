7c1027186826997b0d524753e218a54c36927e73
Keyla Mariela Carrera Prieto <marcarrera00mail.com>
2022-07-15 22:19:45 -0500

    Vista Mi Avance Fade Effect en desarrollo

---
+++ assets/js/extensions/mi_avance.js
@@ -0,0 +1,27 @@
+window.onscroll = function(){miFuncion()};
+
+var basico1 = document.getElementById("basico1");
+var basico2 = document.getElementById("basico2");
+var basico3 = document.getElementById("basico3");
+var basico4 = document.getElementById("basico4");
+
+var dis_basico1, dis_basico2, dis_basico3, dis_basico4;
+
+function miFuncion(){
+    dis_basico1 = window.innerHeight - basico1.getBoundingClientRect().top;
+    if(dis_basico1 >= 200){
+        basico1.classList.add("efecto-basico1");
+    }
+    dis_basico2 = window.innerHeight - basico2.getBoundingClientRect().top;
+    if(dis_basico2 >= 200){
+        basico2.classList.add("efecto-basico2");
+    }
+    dis_basico3 = window.innerHeight - basico3.getBoundingClientRect().top;
+    if(dis_basico3 >= 200){
+        basico3.classList.add("efecto-basico3");
+    }
+    dis_basico4 = window.innerHeight - basico4.getBoundingClientRect().top;
+    if(dis_basico4 >= 200){
+        basico4.classList.add("efecto-basico4");
+    }
+}
